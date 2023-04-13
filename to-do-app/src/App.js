ca794b6d8877dec1c2cca9fd85e9970e3b2f6664
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-12 20:24:03 -0500

    added navbar

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <NavBar />
+      <div className='container'>
+        <h1>Hello World</h1>
+      </div>
+
     </div>
   )
 }
---
+++ to-do-app/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+
+  return (
+    <div>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+            <a href="/" className="navbar-brand">To Do List</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
