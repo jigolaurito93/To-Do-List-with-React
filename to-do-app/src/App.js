f265e848c65c42d3186b905a94f683f2363511c1
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-12 22:27:47 -0500

    added a toDoForm

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar'
+import ToDoForm from './components/ToDoForm'
 
 export default function App() {
+      // toDos is the current list of toDos
+      // setToDos is a function that sets the toDos state
+      // useState is a hook that returns the current toDos state
+  const [toDos, setToDos] = useState([]);
+  const addToList = task => {
+    console.log(task);
+    setToDos([...toDos, task]);
+  }
+
   return (
     <div>
       <NavBar />
       <div className='container'>
-        <h1>Hello World</h1>
+        <ToDoForm/>
       </div>
 
     </div>
--- to-do-app/src/components/ToDoForm.jsx
+++ to-do-app/src/components/ToDoForm.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function ToDoForm() {
   return (
-    <div>
-      
-    </div>
+    <form action="">
+        <h1 className='text-center'>Add A Task</h1>
+        <div className="form-group">
+            <label htmlFor="task">Enter task</label>
+            <input type="text" name="task" className='form-control' id="" placeholder='Enter a Task...'/>
+            <input type="submit" value="Add Task" className='btn btn-outline-success w-100 mt-3' />
+        </div>
+    </form>
   )
 }
